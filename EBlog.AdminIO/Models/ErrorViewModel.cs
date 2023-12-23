d444440558611e5b780182a8d052159310a9606b
kocserdar <serdkoc@gmail.com>
2023-12-23 17:52:00 +0300

    base

---
+++ EBlog.AdminIO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EBlog.AdminIO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
